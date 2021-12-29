f3d91615587e5841a14385d408ef716d3e129e42
rofifaghna <85244975+rofifaghna@users.noreply.github.com>
2021-12-29 16:51:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -221,7 +221,7 @@ include "config/fungsi_ago.php";
 													<!-- Ambil sj -->
 <!-- =========================================================================================================================================== -->
 		<!--basic scripts-->
-		<!--[if !IE]>-->
+		<!--[if !IE]>--> 
 		<!--<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>-->
 		<!--<![endif]-->
 		<!--[if IE]>
