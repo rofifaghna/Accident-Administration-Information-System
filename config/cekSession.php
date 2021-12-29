adcc45d711bce6ae364bc1dda92b30a379f9aa15
rofifaghna <85244975+rofifaghna@users.noreply.github.com>
2021-12-29 16:46:07 +0700

    Update cekSession.php

--- config/cekSession.php
+++ config/cekSession.php
@@ -2,6 +2,6 @@
 	session_start();
 	if($_SESSION['s_nama']=='' && $_SESSION['s_pass']=='' && 
         $_SESSION['s_level']=='' && $_SESSION['s_user']=='') {
-		header("location:login.php");
+		header("location:login.php"); 
 	}
 ?>
